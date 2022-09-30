99e9276f3a291db5163b7c1a2f61087f8979e960
Joonoo <inyoung852@gmail.com>
2022-09-30 17:12:49 +0900

    merge class hello

--- GitProject0930/src/kosta/hello.java
+++ GitProject0930/src/kosta/hello.java
@@ -3,8 +3,21 @@ package kosta;
 public class hello {
     String name;
     String title;
-    name = "안녕하세요";
-    title = "조";
-    System.out.println("name" + name);
-    System.out.println("title" + title);
+    
+   
+    
+
+	public hello(String name, String title) {
+		super();
+		this.name = name;
+		this.title = title;
+	}
+	
+	public void print() {
+	
+		name = "안녕하세요";
+	    title = "조";
+	    System.out.println("name" + name);
+	    System.out.println("title" + title);
+	}
 }
