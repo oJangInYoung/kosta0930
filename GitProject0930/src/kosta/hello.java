a22fdb2474ba4b23767154140ee8323dcc61c098
JinWon Oh <dhwlsdnjs55@naver.com>
2022-09-30 17:06:54 +0900

    update hello.java

--- GitProject0930/src/kosta/hello.java
+++ GitProject0930/src/kosta/hello.java
@@ -3,5 +3,8 @@ package kosta;
 public class hello {
     String name;
     String title;
-
+    name = "안녕하세요";
+    title = "조";
+    System.out.println("name" + name);
+    System.out.println("title" + title);
 }
